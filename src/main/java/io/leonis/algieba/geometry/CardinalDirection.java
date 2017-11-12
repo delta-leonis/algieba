b2e4325d719a982f33058713d26e1179495e0be7
Jeroen de Jong <thumbnail@users.noreply.github.com>
2017-11-12 14:42:36 +0100

    Intercardinal directions (#3)
    * Add intercardinal directions
    
    * Add method to find CardinalDirection based on onrientation
    
    * Add getter for CardinalDirection in Orientation
    
    * Add more parentheses

--- src/main/java/io/leonis/algieba/geometry/CardinalDirection.java
+++ src/main/java/io/leonis/algieba/geometry/CardinalDirection.java
@@ -6,22 +6,54 @@ package io.leonis.algieba.geometry;
  * This enumeration enumerates the principal cardinal directions.
  *
  * @author Rimon Oz
+ * @author Jeroen de Jong
  */
 public enum CardinalDirection {
   /**
    * North cardinal direction.
    */
   NORTH,
+
+  /**
+   * North-east cardinal direction.
+   */
+  NORTH_EAST,
+
   /**
    * East cardinal direction.
    */
   EAST,
+
+  /**
+   * South-east cardinal direction.
+   */
+  SOUTH_EAST,
+
   /**
    * South cardinal direction.
    */
   SOUTH,
+
+  /**
+   * South-west cardinal direction.
+   */
+  SOUTH_WEST,
+
   /**
    * West cardinal direction.
    */
-  WEST
+  WEST,
+
+  /**
+   * North-west cardinal direction.
+   */
+  NORTH_WEST;
+
+  /**
+   * @param orientation orientation in degrees where north is 0.
+   * @return he cardinal direction based on the provided orientation.
+   */
+  public static CardinalDirection from(double orientation) {
+    return values()[(int)((orientation % 360) / 45)];
+  }
 }
--- src/main/java/io/leonis/algieba/geometry/Orientation.java
+++ src/main/java/io/leonis/algieba/geometry/Orientation.java
@@ -14,4 +14,11 @@ public interface Orientation {
    * @return The orientation of the object.
    */
   double getOrientation();
+
+  /**
+   * @return The {@link CardinalDirection} based on {@link #getOrientation()}.
+   */
+  default CardinalDirection getCardinalDirection() {
+    return CardinalDirection.from(this.getOrientation());
+  }
 }
